b141718840f82cd9648d05b5bde8ccb955ac6f78
ccarcaci <claudiocarcaci@gmail.com>
2020-12-09 22:10:41 +0100

    Add security map exploding implementation

--- src/spec/parts/paths/securityMapper.ts
+++ src/spec/parts/paths/securityMapper.ts
@@ -1,13 +1,34 @@
 import { OpenAPIV3 } from 'openapi-types'
+import { KeySecuritySchemeType } from './types/KeySecuritySchemeType'
 
 export type KeySecuritySchemesType = { [key: string]: OpenAPIV3.ReferenceObject | OpenAPIV3.SecuritySchemeObject }
 
+const explodeSecuritySchemes = (
+  explodingSecurity?: OpenAPIV3.SecurityRequirementObject[],
+  securitySchemes?: KeySecuritySchemesType): KeySecuritySchemeType[] => {
+  if(!explodingSecurity) { return [] }
+  if(!securitySchemes) { return [] }
+
+  return explodingSecurity.map((securityRequirement) =>
+    Object.keys(securityRequirement).reduce((previousValue, securityRequirementKey) => ({
+      ...previousValue,
+      // eslint-disable-next-line security/detect-object-injection
+      [securityRequirementKey]: securitySchemes[securityRequirementKey],
+    }), {}))
+}
+
 const securityMapper = (
   spec: OpenAPIV3.Document,
-  operationObject: OpenAPIV3.OperationObject): KeySecuritySchemesType | undefined => {
-  operationObject
+  operationObject: OpenAPIV3.OperationObject): KeySecuritySchemesType[] | undefined => {
+  let securitySchemes: KeySecuritySchemesType[] = []
 
-  return spec.components?.securitySchemes
+  securitySchemes = explodeSecuritySchemes(operationObject.security, spec.components?.securitySchemes)
+
+  if(securitySchemes.length <= 0) {
+    securitySchemes = explodeSecuritySchemes(spec.security, spec.components?.securitySchemes)
+  }
+
+  return securitySchemes
 }
 
 export { securityMapper }
--- test/spec/parts/paths/securityMapper.test.ts
+++ test/spec/parts/paths/securityMapper.test.ts
@@ -42,31 +42,95 @@ describe('Extract Security Specific to Operation', () => {
       },
     } as unknown as OpenAPIV3.Document
 
-    const operationObject: OpenAPIV3.OperationObject = { }
+    const operationObject: OpenAPIV3.OperationObject = {}
 
-    expect(securityMapper(spec, operationObject)).toStrictEqual({
-      petstore_auth: {
-        type: 'oauth2',
-        flows: {
-          implicit: {
-            authorizationUrl: 'http://example.org/api/oauth/dialog',
-            scopes: {
-              'write:pets': 'modify pets in your account',
-              'read:pets': 'read your pets',
+    expect(securityMapper(spec, operationObject)).toStrictEqual([
+      {
+        petstore_auth: {
+          type: 'oauth2',
+          flows: {
+            implicit: {
+              authorizationUrl: 'http://example.org/api/oauth/dialog',
+              scopes: {
+                'write:pets': 'modify pets in your account',
+                'read:pets': 'read your pets',
+              },
             },
           },
         },
       },
-      http_auth: {
-        type: 'http',
-        description: 'HTTP authorization',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
+      {
+        http_auth: {
+          type: 'http',
+          description: 'HTTP authorization',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
       },
-    })
+    ])
   })
 
-  // test('Security restrictions applies at the same time')
+  // eslint-disable-next-line max-lines-per-function
+  test('Multiple security restrictions', () => {
+    const spec: OpenAPIV3.Document = {
+      security: [
+        {
+          petstore_auth: [
+            'write:pets',
+            'read:pets',
+          ],
+          http_auth: [],
+        },
+      ],
+      components: {
+        securitySchemes: {
+          petstore_auth: {
+            type: 'oauth2',
+            flows: {
+              implicit: {
+                authorizationUrl: 'http://example.org/api/oauth/dialog',
+                scopes: {
+                  'write:pets': 'modify pets in your account',
+                  'read:pets': 'read your pets',
+                },
+              },
+            },
+          },
+          http_auth: {
+            type: 'http',
+            description: 'HTTP authorization',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+      },
+    } as unknown as OpenAPIV3.Document
+
+    const operationObject: OpenAPIV3.OperationObject = {}
+
+    expect(securityMapper(spec, operationObject)).toStrictEqual([
+      {
+        petstore_auth: {
+          type: 'oauth2',
+          flows: {
+            implicit: {
+              authorizationUrl: 'http://example.org/api/oauth/dialog',
+              scopes: {
+                'write:pets': 'modify pets in your account',
+                'read:pets': 'read your pets',
+              },
+            },
+          },
+        },
+        http_auth: {
+          type: 'http',
+          description: 'HTTP authorization',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    ])
+  })
 
   // test('Apply only a subset of oauth2 scopes')
 
